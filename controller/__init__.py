03f7e4afbf002ff46dce63791089ac755d0d2a5b
diego <diego@localhost.localdomain>
2016-07-23 02:05:32 -0400

    Defining Model

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,14 +1,1 @@
-# *-* coding: utf-8 *-*
-
-import cotlib
-
-'''
-Environment
-===========
-
-This class serves as a workspace. Once instantiated,it can be filled with species and reactions.
-
-'''
-class environment:
-    a = 0
-
+#HC SVNT DRACONES
--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,16 @@
-#HC SVNT DRACONES
+# *-* coding: utf-8 *-*
+
+import cotlib
+
+'''
+Model
+===========
+
+This class serves as a 'logical' workspace. Once instantiated,it can be filled with species and reactions.
+
+'''
+
+class Model:
+    Global_species = []
+    Global_reaction_networks = []
+    Global_reactions = []
