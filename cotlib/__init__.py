82276d21e0d2ba09b3f4bdb73061681ec964df6f
diego <diego@localhost.localdomain>
2016-07-25 01:45:47 -0400

    isSM() es incorporado como método de la clase renet

--- cotlib/__init__.py
+++ cotlib/__init__.py
@@ -1,13 +1,28 @@
 # *-* coding: utf-8 *-*
-
 """
 COTlib
 ======
 
-Provides three classes needed to build a reaction network.
+Python package for complex systems analysis through Chemical Organization
+Theory.
+
+This package provides three classes needed to build a reaction network:
+    1.- species:    Species
+    2.- reaction:   Reactions
+    3.- renet:      Reaction Networks
+
+This package also provides two global sets containing all the species
+and reactions created, called M and R respectively.
 
 """
 
+import cotlib
+import cvxopt as co
+import numpy as np
+
+
+M = set()
+R = set()
 
 
 class species:
@@ -18,10 +33,36 @@ class species:
         else:
             raise TypeError
 
+class reaction:
+    MyOwners = set()
 
 class renet:
     X = set()
     R_x = set()
+    S_x = np.array([])
 
-class reaction:
-    MyOwners = set()
+    def isSM(self, S):
+        #M should be np.array
+        vars = np.shape(S)[1]
+        eqs1 = np.shape(S)[0]
+        eqs2 = vars
+        I = np.identity(vars)
+        A = S
+        A = np.concatenate((A, I), axis=0)
+        A = -A
+        b0 =  np.zeros(eqs1)
+        b1 = -np.ones(eqs2)
+        b = np.concatenate((b0, b1), axis=0)
+        c = np.ones(vars)
+
+        A = co.matrix(A)
+        b = co.matrix(b)
+        c = co.matrix(c)
+
+        co.solvers.options['show_progress']=False
+        sol=co.solvers.lp(c,A,b)
+
+        return bool(sol['x'])
+
+
+
